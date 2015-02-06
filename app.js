0d0481d63a1ae617545aefbb1f550885e9d8cdc6
Arebeth Pease <arebethpease@gmail.com>
2015-02-06 09:10:56 -0800

    refactored code and debugged determineWinner function.

--- app.js
+++ app.js
@@ -8,41 +8,33 @@ $(document).ready(function() {
  	var playerOneScore = 0;
  	var playerTwoScore = 0;
  	$(".row-plyr1").addClass("currentTurn");
+ 	var questionCounter = 0;
 
- 	function identifyTurn () {  //check player turns by toggling class -- keeping zombie code to show refactor
-			//if (counter%2===0) {
-				// turn on currentTurn class for player one, turn it off for player 2
+ 	function identifyTurn () {  //check player turns by toggling class 
 				$(".row-plyr1").toggleClass("currentTurn"); //chose to highlight the player instead of alerting player rotation
 				$(".row-plyr2").toggleClass("currentTurn");
-			//} else {
-				// turn on currentTurn class for player two, turn it off for player one
-			//	$(".row-plyr1").toggleClass("currentTurn");
-			//	$(".row-plyr2").toggleClass("currentTurn");
-			//}
  	}
 
  	function answerIsCorrect (answer) {  //used method extraction to refactor a confusing piece of code
-		return $("input[type=radio]:checked").next().text() === answer; //return the text of the label next to the radio
+		return $("input[type=radio]:checked").next().text() === answer; //return the text of the span next to the radio
  	}
 
- 	function disableCell(cell) { //disables cell and adds "alreadyAnswered class"
+ 	function disableCell(cell) { //disables cell and adds "alreadyAnswered" class
  		$(cell).addClass("alreadyAnswered");
+ 		questionCounter++;
  	}
 
- 	// function determineWinner () {  //tried to add a function to determineWinner, but ran out of time to debug.
- 	// 	var continueGame = false;
- 	// 	$("td").forEach(function () {	
- 	// 		if ($(this).hasClass("alreadyAnswered")) {
- 				
- 	// 		} else {
- 	// 			continueGame = true;
- 	// 		} 
- 	// 	});
- 	// 	if (continueGame === false) {
- 	// 		alert("The game is over.");
- 	// 	}	
- 	// }
- 		
+ 	function determineWinner () { //if all questions have been answered, determine the winner and stop the game.
+ 		if (questionCounter === 25) {
+			if (playerOneScore > playerTwoScore) {
+				alert("The game is over. Player One is the winner.");
+			} else if (playerOneScore < playerTwoScore) {
+				alert("The game is over. Player Two is the winner.");
+			} else {
+				alert("The game is over. Players are tied. Everyone is a winner.");
+			}
+ 		}
+ 	}	
 //done defining functions
 
 //begin game play
@@ -54,10 +46,8 @@ $(document).ready(function() {
 		disableCell(this); //disables the cell to prevent it from being clicked again
 		var number = $(this).attr("data-id"); 
 		var amount = parseInt($(this).attr("data-amt")); 
-		//alert (typeof amount); -- keeping this zombie code for posterity as a way to remember typeof
 
-
-		data.forEach(function (val) {  //loop through data array (in data,js) and on the instance of each object...
+		data.forEach(function (val) {  //loop through data array (in data.js) and on the instance of each object...
 
 			if (parseInt(number) === val.id) {  //find the object whose id value = the integer from the data-id (in page3.html)
 				var answer1 = ("<label> <input type='radio' name='answer' class='radio'> <span>" + val.mcAnswer1 + "</span></label><br>");
@@ -72,17 +62,11 @@ $(document).ready(function() {
 				$("#choice p").append("<span class='question'>" + val.question+ "</span>"); //then add the value to the question key from the object
 				$("#choice p").append(form); //then add form
 
-				form.empty(); //empty any previous values -- keeping zombie code to show refactor
-				//var radio1 = $("<input type='radio' name='answer' class='radio'>"); //moved variables to prevent radio click position from locking
- 				//var radio2 = $("<input type='radio' name='answer' class='radio'>");
- 				//var radio3 = $("<input type='radio' name='answer' class='radio'>");
-				//form.append(radio1);
-				form.append(answer1);	//add answer 1 to the form		
-				//form.append(radio2);
-				form.append(answer2);   //add answer 2 to the form	
-				//form.append(radio3);
-				form.append(answer3);   //add answer 3 to the form	
-				form.append(submit);    //add submit button to the form
+				form.empty(); //empty any previous values 
+				form.append(answer1);	//add answers and submit button to the form		
+				form.append(answer2);   	
+				form.append(answer3);   
+				form.append(submit);   
    
 				answer = val.realAnswer;  //set answer to the realAnswer value in the object
 			}
@@ -92,7 +76,7 @@ $(document).ready(function() {
 			e.preventDefault();                     //prevent immediate action
 
 	 		if(answerIsCorrect(answer)){            //if they chose the correct answer 
-	 			alert("You're correct! It's your turn again!");
+	 			alert("You are correct!");
 	 			if (counter%2===0) {                //alert it's correct and give points to the correct player
 	 				playerOneScore += amount;       
 	 				
@@ -101,15 +85,9 @@ $(document).ready(function() {
 	 			}		
 	 		}
 	 		else{                                   //if they choose incorrectly, alert and add to counter to change turns. game designed not to deduct points.
-	 			alert("Sorry, that was incorrect. The correct answer is " + answer);
+	 			alert("Sorry, that was incorrect. \n\nThe correct answer is " + answer);
 	 			counter++;
-	 			identifyTurn(); //player rotation function - ONLY CALL WHEN ANSWER IS INCORRECT
-	 			if(counter % 2 === 0){
-	 				alert("It is now player one's turn!");
-	 			}
-	 			else{
-	 				alert("It is now player two's turn!");
-	 			}		
+	 			identifyTurn(); //player rotation function - ONLY CALL WHEN ANSWER IS INCORRECT		
 	 		}
 
 	 		$(".row-plyr1 .score").text(playerOneScore);  //scorekeeping
@@ -118,7 +96,7 @@ $(document).ready(function() {
 	 		$("#choice p").fadeOut(400, function () {     //once turn has changed, fade out answer and then fade board back in
 	 			$(".board").fadeIn(400, function (){});
 	 		});
-	 	//determineWinner(); -- determineWinner function was removed until it can be debugged.
+	 	determineWinner(); //check if game is over
 		});	//end of on-click for submit button
 	});
 
